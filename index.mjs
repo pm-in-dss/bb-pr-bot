b2c409b45d238411d2e8285aacc3366936cb3cd0
Pomba <pombam@highground.life>
2025-06-12 01:18:35 +0545

    init

---
+++ index.mjs
@@ -0,0 +1,63 @@
+#!/usr/bin/env node
+
+const {
+	BITBUCKET_REPO_SLUG,
+	BITBUCKET_WORKSPACE,
+	PR_BOT_TOKEN,
+	PR_BOT_SOURCE_BRANCH = "main",
+	PR_BOT_DESTINATION_BRANCH = "prod",
+	PR_BOT_PR_TITLE = "Merge staging to production",
+} = process.env
+
+function log(message, level = "info") {
+	console[level]("[bb-pr-bot] " + message)
+}
+
+if (!BITBUCKET_REPO_SLUG || !BITBUCKET_WORKSPACE || !PR_BOT_TOKEN) {
+	log("Missing environment variables: $BITBUCKET_REPO_SLUG, $BITBUCKET_WORKSPACE, $PR_BOT_TOKEN", "error")
+	process.exit(1);
+}
+
+
+async function doFetch(path, body) {
+	log(`Sending request to: ${path} with body: ${JSON.stringify(body, null, 2)}`)
+
+	try {
+		const url = `https://api.bitbucket.org/2.0/repositories/${BITBUCKET_WORKSPACE}/${BITBUCKET_REPO_SLUG}${path}`;
+
+		const response = await fetch(url, {
+			method: "POST",
+			headers: {
+				"Authorization": `Bearer ${PR_BOT_TOKEN}`,
+				"Accept": "application/json",
+				"Content-Type": "application/json"
+			},
+			body: JSON.stringify(body)
+		});
+
+		if (!response.ok) {
+			throw new Error(response.statusText);
+		}
+
+		const ret = await response.json();
+
+		log("Request successful")
+
+		return ret;
+	} catch (error) {
+		log(error.message, "error")
+		process.exit(1);
+	}
+}
+
+const pr = await doFetch("/pullrequests", {
+	"title": PR_BOT_PR_TITLE,
+	"source": {
+		"branch": { "name": PR_BOT_SOURCE_BRANCH }
+	},
+	"destination": {
+		"branch": { "name": PR_BOT_DESTINATION_BRANCH }
+	}
+})
+
+await doFetch(`/pullrequests/${pr.id}/merge`, { "close_source_branch": false })
